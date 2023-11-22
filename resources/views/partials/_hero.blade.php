5370c9a288c4f3857b6c2fe96a186a50b830a3ae
mdtajulislam22 <mdtajulislamr22@gmail.com>
2023-11-23 01:22:55 +0600

    landing page banner is completed

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,21 +1,24 @@
 <!-- Hero -->
-<section class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4">
+<section class="relative h-72 bg-green-300 flex flex-col justify-center align-center text-center space-y-4 mb-4">
 <div
     class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
-    style="background-image: url('images/laravel-logo.png')"
+    style="background-image: url('images/Banner/background2.png')"
 ></div>
 
 <div class="z-10">
-    <h1 class="text-6xl font-bold uppercase text-white">
-        Lara<span class="text-black">Gigs</span>
+    <h1 class="text-6xl font-bold uppercase text-red-700">
+        J<span class=" text-5xl text-black">obatho</span>
     </h1>
-    <p class="text-2xl text-gray-200 font-bold my-4">
-        Find or post Laravel jobs & projects
+    <p class="text-2xl text-white font-bold my-4">
+        ---- Job-At-Home ----
+    </p>
+    <p class="text-2xl text-white my-4">
+        Find your job at home, office and building
     </p>
     <div>
         <a href="/register"
             class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-            >Sign Up to List a Gig</a>
+            >Sign Up to find job</a>
     </div>
 </div>
 </section>
---
+++ resources/views/partials/_main_banner.blade.php
@@ -0,0 +1,113 @@
+{{-- first banner --}}
+
+{{-- <div class="relative bg-green-300 overflow-hidden">
+  <!-- Background Phone Layer with Low Opacity -->
+  <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('images/Banner/background1.png'); opacity: 0.3;"></div>
+
+  <!-- Main Content Container -->
+  <div class="relative z-10 container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+    <!-- Three Sections in a Row -->
+    <div class="flex flex-wrap -mx-4">
+      <!-- Left Phone -->
+      <div class="w-full md:w-1/3 px-4">
+        <img src="images/Banner/door.png" alt="Left Phone" class="rounded-lg shadow-xl mb-4">
+      </div>
+
+      <!-- Center Section -->
+      <div class="w-full md:w-1/3 px-4 flex flex-col items-center justify-center">
+        <!-- First Row -->
+        <div class="mb-4 text-white text-2xl">JOBATHO</div>
+
+        <!-- Second Row -->
+        <div class="mb-4 text-white text-lg">Job-At-Home</div>
+
+        <!-- Third Row with Button -->
+        <div class="text-center">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign Up</button>
+        </div>
+      </div>
+
+      <!-- Right Phone -->
+      <div class="w-full md:w-1/3 px-4">
+        <img src="images/Banner/anti.png" alt="Right Phone" class="rounded-lg shadow-xl mb-4">
+      </div>
+    </div>
+  </div>
+</div> --}}
+
+
+
+{{-- second banner --}}
+
+{{-- <div class="relative bg-green-300 overflow-hidden">
+  <!-- Background Phone Layer with Low Opacity -->
+  <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('images/Banner/background1.png'); opacity: 0.3;"></div>
+
+  <!-- Main Content Container -->
+  <div class="relative z-10 container mx-auto py-8 px-4 sm:px-6 lg:px-8 max-w-screen-md opacity-0.3">
+    <!-- Three Sections in a Column -->
+    <div class="flex flex-col items-center">
+      <!-- Center Section -->
+      <div class="mb-4 text-white text-2xl">Your Content Here</div>
+      <div class="mb-4 text-white text-lg">Your Content Here</div>
+
+      <!-- Third Row with Button -->
+      <div class="mb-4 text-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Call to Action</button>
+      </div>
+
+      <!-- Two Phones in the Bottom Row -->
+      <div class="flex justify-between w-full">
+        <!-- Left Phone -->
+        <img src="images/Banner/door1.png" alt="Left Phone" class="w-1/3 rounded-lg shadow-xl mb-4">
+
+        <!-- Right Phone -->
+        <img src="images/Banner/anti.png" alt="Right Phone" class="w-1/3 rounded-lg shadow-xl mb-4">
+      </div>
+    </div>
+  </div>
+</div> --}}
+
+
+
+{{-- third banner --}}
+<div class="relative banner">
+    <div class="absolute top-0 left-0 w-full h-full bg-green-300 opacity-25" >
+  <img src="images/Banner/background2.png" alt="images/Banner/background2.png" class="w-full h-full object-cover " style="opacity:0.15;"/>
+</div>
+
+  <div class="flex items-center justify-between ">
+    <div class="flex flex-col items-center ">
+      <img src="images/Banner/door1.png" alt="" class="w-40 h-40 object-cover" />
+      {{-- <img src="images/Banner/anti.png" alt="" class="w-40 h-40 object-cover" /> --}}
+    </div>
+
+    {{-- <div class="text-center">
+      <h2 class="text-3xl font-bold text-white leading-tight">Your App Title</h2>
+      <p class="text-lg text-gray-500">Your App Description</p>
+      <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">Download App</button>
+    </div> --}}
+    <div class="z-10">
+        <h1 class="text-6xl font-bold uppercase text-white">
+            Lara<span class="text-black">Gigs</span>
+        </h1>
+        <h4 class="text-2xl text-gray-200 font-bold my-4">
+            Find or post Laravel jobs & projects
+        </h4>
+        <p class="text-2xl text-gray-200 font-bold my-4">
+            Find or post Laravel jobs & projects
+        </p>
+        <div>
+            <a href="/register"
+                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+            >Sign Up to List a Gig</a>
+        </div>
+    </div>
+
+    <div class="flex flex-col items-center">
+      {{-- <img src="images/Banner/door1.png" alt="" class="w-40 h-40 object-cover" /> --}}
+      <img src="images/Banner/anti.png" alt="" class="w-40 h-40 object-cover" />
+    </div>
+  </div>
+</div>
+
